c8ba07717556ecbb0b3a7c2d2467094d8f8e5abf
MOYO2702 <olayemisimoyo30@gmail.com>
2024-12-26 17:18:09 +0400

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,11 @@
+// Explore Product button click event
+function exploreProduct() {
+  alert('Explore the product button clicked!');
+  window.location.href = "#";  // Link to product page
+}
+
+// Free Trial button click event
+function startTrial() {
+  alert('Start Free Trial button clicked!');
+  window.location.href = "#";  // Link to trial signup page
+}
